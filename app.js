16ab64927681915ec6fe31fd1735e9804d1b6a3f
Lai Trong Dao_51200682 <51200682@stu.hcmut.edu.vn>
2018-01-26 00:20:29 +0700

    init

---
+++ app.js
@@ -0,0 +1,50 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+var fs = require('fs');
+var request = require('request');
+var coinlist = [];
+fs.readFile('coinlist.txt', function (err, data) {
+  if (err) throw err;
+  coinlist = JSON.parse(data);
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    var message = session.message.text;
+    var coinName = coinlist[message.toUpperCase()];
+    if (!coinName) coinName = message.toLowerCase();
+    request('https://api.coinmarketcap.com/v1/ticker/'+coinName, function (error, res, body) {
+      var response = '';
+      if (res.statusCode === 200) {
+        var data = JSON.parse(body)[0];
+        response = data.name + '\n\n';
+        response += 'USD: ' + data.price_usd + '$\n\n';
+        response += 'BTC: ' + data.price_btc + '\n\n';
+        response += '1h change: ' + data.percent_change_1h + '%\n\n';
+        response += '24h change: ' + data.percent_change_24h +'%\n\n';
+        response += 'Rank: ' + data.rank + '\n';
+
+      }
+      else {
+        response = 'Cannot find the coin, please check if it correct!'
+      }
+      session.send(response);
+    });
+
+
+});
---
+++ test.js
@@ -0,0 +1,18 @@
+var request = require('request');
+var fs = require('fs');
+request('https://api.coinmarketcap.com/v1/ticker/?limit=1500', function (error, response, body) {
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log(JSON.parse(body).length);
+  data = JSON.parse(body);
+  var coinArr = {};
+  for (var i = 0; i < data.length; i++) {
+    coinArr[data[i].symbol] = data[i].id;
+  }
+  fs.writeFile("coinlist.txt", JSON.stringify(coinArr), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+});
+});
