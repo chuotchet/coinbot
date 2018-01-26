3eb8ceb0a4d0ca25ed667245d223da1986c815da
chuotchet <chuotchet94@gmail.com>
2018-01-26 11:34:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
 
+server.get('/', function (req, res, next) {
+  res.send('ahihi');
+  return next();
+});
+
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
     var message = session.message.text;
