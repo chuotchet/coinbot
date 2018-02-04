5757cf953705b6f146b2e060dcd78aaf91514989
Lai Trong Dao_51200682 <51200682@stu.hcmut.edu.vn>
2018-02-04 10:03:09 +0700

    update

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var fs = require('fs');
-request('https://api.coinmarketcap.com/v1/ticker/?limit=1500', function (error, response, body) {
+request('https://api.coinmarketcap.com/v1/ticker/?limit=2000', function (error, response, body) {
   // console.log('body:', body); // Print the HTML for the Google homepage.
   console.log(JSON.parse(body).length);
   data = JSON.parse(body);
